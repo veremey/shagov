d0bd12ae23b6c4046f13b4ff64d4719bd65fdc54
veremey <veremey@i.ua>
2015-03-03 04:07:36 +0200

    finish

--- js/common.js
+++ js/common.js
@@ -32,6 +32,30 @@ $(".accordion__link").click(function (){
 	return false;
 });
 
+ function tab() {
+        $(".js-tab").each(function(){
+
+            var tab_link = $(this).find("a");
+            var tab_cont = $(this).parents(".js-tab-group").find(".js-tab-cont");
+            tab_cont.hide();
+            $(this).parents(".js-tab-group").find(".js-tab1").show();
+
+            tab_link.bind("click", function() {
+                var index = $(this).attr("href");
+                tab_link.removeClass("is-active");
+                tab_link.parent().removeClass("is-active");
+                $(this).addClass("is-active");
+                $(this).parent().addClass("is-active");
+                tab_cont.hide();
+                $(this).parents(".js-tab-group").find("."+index).slideDown("fast");
+                return false;
+            });
+        });
+    }
+    tab();
+
+
+
 });
 
 
